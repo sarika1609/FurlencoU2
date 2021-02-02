ff5c92ef43f10efdda25520250e41001cd1bb160
Chaithanya Gollapalli <chaitanyagcg.01@gmail.com>
2021-02-02 21:58:31 +0530

    Initial Fragments

---
+++ Furlenco/app/src/main/java/com/example/furlenco/InitialScreenFragments/FragmentOne.java
@@ -0,0 +1,70 @@
+package com.example.furlenco.InitialScreenFragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.furlenco.MainActivity;
+import com.example.furlenco.R;
+
+public class FragmentOne extends Fragment {
+
+    private Button mBtnSkip;
+    private Button mBtnNext;
+    private MainActivity mainActivity;
+
+    public static FragmentOne newInstance() {
+        FragmentOne fragmentOne = new FragmentOne();
+        return fragmentOne;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mBtnSkip = view.findViewById(R.id.btnSkip);
+        mBtnNext = view.findViewById(R.id.btnNext);
+
+
+
+//        mBtnNext.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                (MainActivity(getActivity())
+//            }
+//        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+//        mBtnSkip.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("tag","tag");
+//                FragmentFive.newInstance();
+////                mainActivity.launchFragment("Skip");
+//            }
+//        });
+    }
+}
--- Furlenco/app/src/main/java/com/example/furlenco/MainActivity.java
+++ Furlenco/app/src/main/java/com/example/furlenco/MainActivity.java
@@ -1,14 +1,92 @@
 package com.example.furlenco;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.furlenco.InitialScreenFragments.FragmentFive;
+import com.example.furlenco.InitialScreenFragments.FragmentFour;
+import com.example.furlenco.InitialScreenFragments.FragmentOne;
+import com.example.furlenco.InitialScreenFragments.FragmentThree;
+import com.example.furlenco.InitialScreenFragments.FragmentTwo;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private FragmentManager fragmentManager;
+    private String tag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        setViewPagerAdapter();
     }
+
+    private void initViews() {
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+        fragmentManager = getSupportFragmentManager();
+    }
+
+    private void setViewPagerAdapter() {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),
+                FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter{
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return FragmentOne.newInstance();
+                case 1:
+                    return FragmentTwo.newInstance();
+                case 2:
+                    return FragmentThree.newInstance();
+                case 3:
+                    return FragmentFour.newInstance();
+                case 4:
+                    return FragmentFive.newInstance();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+
+//    public void launchFragment(String tag) {
+//        this.tag = tag;
+//        if (tag.equals("Skip")) {
+//            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//            FragmentFive fragmentFive = new FragmentFive();
+//            fragmentTransaction.replace(R.id.flContainer, fragmentFive, "FragmentFive").addToBackStack("FragmentFive").commit();
+//        }
+//    }
+//
+//    public void openFifthFragment(CommunicationListener listener) {
+//        if (listener != null) {
+//            listener.onDataPassed(tag);
+//        }
+//    }
 }
