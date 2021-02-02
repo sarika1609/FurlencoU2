0ed7c119d0eec906933b0e7c1a2456729672d656
sarika1609 <sarika.thota1992@gmail.com>
2021-02-02 13:08:22 +0530

    added home page bottom bar

--- Furlenco/app/src/main/java/com/example/furlenco/MainActivity.java
+++ Furlenco/app/src/main/java/com/example/furlenco/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.furlenco;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.botton_bar_layout);
+
     }
+
 }
